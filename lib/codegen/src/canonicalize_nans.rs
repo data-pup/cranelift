517d7f0157477035b129bd4b5f16debff1154f0c
data-pup <kevinmartinucr@gmail.com>
2018-04-30 15:14:27 -0700

    FIXUP: Adding underscores to avoid lint warning.

--- lib/codegen/src/canonicalize_nans.rs
+++ lib/codegen/src/canonicalize_nans.rs
@@ -4,14 +4,17 @@ use cursor::{Cursor, FuncCursor};
 use ir::Function;
 use timing;
 
+// Canonical NaN values.
+
+
 // TODO: Helper functions should go here.
 
 /// The main NaN-canonicalization pass.
-pub fn do_canonicalize_nans(func: &mut Function) {
+pub fn _do_canonicalize_nans(func: &mut Function) {
     let _tt = timing::canonicalize_nans();
     let mut pos = FuncCursor::new(func);
     while let Some(_ebb) = pos.next_ebb() {
-        while let Some(inst) = pos.next_inst() {
+        while let Some(_inst) = pos.next_inst() {
             // TODO: NaN Canonicalization should go here.
             unimplemented!();
         }
