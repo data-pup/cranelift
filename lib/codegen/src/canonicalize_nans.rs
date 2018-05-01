00e7d4bd9e1862887bcc931d789805084d8834ce
data-pup <kevinmartinucr@gmail.com>
2018-04-30 17:11:43 -0700

    FIXUP COMMIT: Looking into how to confirm if an instruction is a floating point arithmetic operation.

--- lib/codegen/src/canonicalize_nans.rs
+++ lib/codegen/src/canonicalize_nans.rs
@@ -1,22 +1,33 @@
 //! A NaN-canonicalizing rewriting pass.
 
 use cursor::{Cursor, FuncCursor};
-use ir::Function;
+use ir::{Function, Opcode};
 use timing;
 
 // Canonical NaN values.
 
-
 // TODO: Helper functions should go here.
+
+/// Returns true/false based on whether the instruction is a floating-point
+/// arithmetic operation.
+fn is_fp_arith(opcode: Opcode) -> bool {
+    unimplemented!()
+}
 
 /// The main NaN-canonicalization pass.
 pub fn _do_canonicalize_nans(func: &mut Function) {
     let _tt = timing::canonicalize_nans();
     let mut pos = FuncCursor::new(func);
     while let Some(_ebb) = pos.next_ebb() {
-        while let Some(_inst) = pos.next_inst() {
+        while let Some(inst) = pos.next_inst() {
             // TODO: NaN Canonicalization should go here.
-            unimplemented!();
+
+            let data = &pos.func.dfg[inst];
+            let opcode = data.opcode();
+            if is_fp_arith(opcode) {
+                unimplemented!();
+            }
+
         }
     }
 }
