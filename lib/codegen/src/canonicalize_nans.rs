1448b0e0810ffd9b1190f44ae82e6d53a18f705b
data-pup <kevinmartinucr@gmail.com>
2018-05-01 08:41:50 -0700

    FIXUP COMMIT: Cleanup, working on structure for identifying and patching floating point arithmetic operations.

--- lib/codegen/src/canonicalize_nans.rs
+++ lib/codegen/src/canonicalize_nans.rs
@@ -1,33 +1,34 @@
 //! A NaN-canonicalizing rewriting pass.
 
 use cursor::{Cursor, FuncCursor};
-use ir::{Function, Opcode};
+use ir::{Function, Inst, InstructionData, Opcode};
 use timing;
 
-// Canonical NaN values.
-
-// TODO: Helper functions should go here.
-
-/// Returns true/false based on whether the instruction is a floating-point
-/// arithmetic operation.
-fn is_fp_arith(opcode: Opcode) -> bool {
-    unimplemented!()
-}
-
-/// The main NaN-canonicalization pass.
+/// Performs the NaN-canonicalization pass by identifying floating-point
+/// arithmetic operations, and adding instructions to replace the result
+/// with a canonical NaN value if the result of the operation was NaN.
 pub fn _do_canonicalize_nans(func: &mut Function) {
     let _tt = timing::canonicalize_nans();
     let mut pos = FuncCursor::new(func);
     while let Some(_ebb) = pos.next_ebb() {
         while let Some(inst) = pos.next_inst() {
-            // TODO: NaN Canonicalization should go here.
-
-            let data = &pos.func.dfg[inst];
-            let opcode = data.opcode();
-            if is_fp_arith(opcode) {
+            // Determine if `inst` is a floating-point arithmetic operation.
+            if is_fp_arith(&mut pos, inst) {
+                unimplemented!();
+            } else {
                 unimplemented!();
             }
-
         }
     }
+}
+
+/// Returns true/false based on whether the instruction is a floating-point
+/// arithmetic operation.
+fn is_fp_arith(pos: &mut FuncCursor, inst: Inst) -> bool {
+    let data: &InstructionData = &pos.func.dfg[inst];
+    let opcode: Opcode = data.opcode();
+    match opcode {
+        _ => unimplemented!(),
+    }
+    unimplemented!();
 }
