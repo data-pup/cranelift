c3addd87e6c528d2f1622f773c1b6a9f232410ae
data-pup <kevinmartinucr@gmail.com>
2018-05-02 13:12:29 -0700

    FIXUP COMMIT: Moved code responsible for inserting the NaN constant into a helper function.

--- lib/codegen/src/nan_canonicalization.rs
+++ lib/codegen/src/nan_canonicalization.rs
@@ -53,13 +53,44 @@ fn is_fp_arith(pos: &mut FuncCursor, inst: Inst) -> bool {
     }
 }
 
+/// Patch instructions that may result in a NaN result with operations to
+/// identify and replace NaN's with a single canonical NaN value.
+fn add_nan_canon_instrs(pos: &mut FuncCursor, inst: Inst) {
+    // Select the operation's result, and move to the next instruction.
+    // (FIXUP: Is this completely safe to unwrap? Is unwrapping even needed?)
+    let inst_res: Value = pos.func.dfg.first_result(inst);
+    let next_inst: Inst = pos.next_inst().unwrap();
+    let is_nan: Value = pos.ins().fcmp(FloatCC::NotEqual, inst_res, inst_res);
+
+    insert_nan_const(pos, inst);
+    let canon_nan_instr: Inst = pos.prev_inst().unwrap();
+    let canon_nan_res: Value = pos.func.dfg.first_result(canon_nan_instr);
+
+    pos.goto_inst(next_inst);
+    pos.ins().select(is_nan, canon_nan_res, inst_res);
+}
+
+/// Insert the canonical 32-bit or 64-bit NaN constant value at the current
+/// position.
+fn insert_nan_const(pos: &mut FuncCursor, inst: Inst) {
+    match get_nan_type(&pos.func.dfg, inst) {
+        types::F32 => {
+            let canon_nan = Ieee32::with_bits(CANON_32BIT_NAN);
+            pos.ins().f32const(canon_nan);
+        },
+        types::F64 => {
+            let canon_nan = Ieee64::with_bits(CANON_64BIT_NAN);
+            pos.ins().f64const(canon_nan);
+        },
+        _ => unimplemented!() // FIXUP: Should this panic or throw some sort of Error?
+    }
+}
+
 /// Given some instruction that could potentially return a nondeterministic
 /// NaN value, determine if the operation is using 32-bit or 64-bit floating
 /// point numbers, and return the corresponding NaN value.
-/// FIXUP: Not sure if this is the correct prototype for this function.
 fn get_nan_type(dfg: &DataFlowGraph, inst: Inst) -> Type {
     let inst_data: &InstructionData = &dfg[inst];
-
     match *inst_data {
         InstructionData::Unary { arg, .. } => {
             dfg.value_type(arg)
@@ -74,79 +105,4 @@ fn get_nan_type(dfg: &DataFlowGraph, inst: Inst) -> Type {
         },
         _ => unimplemented!(), // FIXUP: What would I do in this case? Error?
     }
-}
-
-/// Patch instructions that may result in a NaN result with operations to
-/// identify and replace NaN's with a single canonical NaN value.
-fn add_nan_canon_instrs(pos: &mut FuncCursor, inst: Inst) {
-    // ----------------------------------------------------------------
-    // Let x be the result to some floating point arithmetic operation.
-    // Add the following instructions after `inst` : (Pseudo-code)
-    // let is_nan = x != x;                          (fcmp)
-    // let canonical_res = is_nan ? CANON_VALUE : x  (select)
-    // ----------------------------------------------------------------
-    // FIXUP: Verbose type annotations are for learning's sake.
-    // ----------------------------------------------------------------
-
-    // Original State:
-    // ----------------------------------------------------------------
-    // x = [floating point arithmetic instruction] <-pos
-    // [next instruction]
-    // ----------------------------------------------------------------
-
-    // Select the operation's result, and move to the next instruction.
-    // (FIXUP: Is this completely safe to unwrap? Is unwrapping even needed?)
-    let inst_res: Value = pos.func.dfg.first_result(inst);
-    let next_inst: Inst = pos.next_inst().unwrap();
-
-    // Insert a comparison to check if the result of the instruction was NaN,
-    // Select a canonical value if NaN, otherwise select the original result.
-    // FIXUP: How/Where to define the constant canonical value?
-    let is_nan: Value = pos.ins().fcmp(FloatCC::NotEqual, inst_res, inst_res);
-
-    // Insert the canonical NaN constant value.
-    match get_nan_type(&pos.func.dfg, inst) {
-        types::F32 => {
-            let canon_nan = Ieee32::with_bits(CANON_32BIT_NAN);
-            pos.ins().f32const(canon_nan);
-        },
-        types::F64 => {
-            let canon_nan = Ieee64::with_bits(CANON_64BIT_NAN);
-            pos.ins().f64const(canon_nan);
-        },
-        _ => unimplemented!() // Should this panic or throw some sort of Error?
-    };
-
-    let canon_nan_instr: Inst = pos.prev_inst().unwrap();
-    let canon_nan_res: Value = pos.func.dfg.first_result(canon_nan_instr);
-    pos.goto_inst(next_inst);
-
-    // pos.insert_inst(canonical_nan_inst);
-    let new_res: Value = pos.ins().select(is_nan, canon_nan_res, inst_res);
-
-    // Current State:
-    // ----------------------------------------------------------------
-    // x = [floating point arithmetic instruction]
-    // is_nan = x != x
-    // canonical_value =  is_nan ? TODO : x
-    // [next instruction]                          <-pos
-    // ----------------------------------------------------------------
-
-    // Move backwards to the last instruction we inserted, so that we can
-    // replace the results of the original instruction with aliases to the
-    // results of the select instruction.
-    let select_inst: Inst = pos.prev_inst().unwrap();
-
-    // Replace the results of the original floating point arithmetic operation
-    // with aliases to the results of the new instruction.
-    // FIXUP: Comments for `replace_with_aliases` mention that `dest_inst` may
-    // need to be removed from the graph. Does this apply in this case?
-
-    // pos.func.dfg.replace_with_aliases(inst, select_inst);
-    // pos.func.dfg.change_to_alias(inst_res, new_res);
-
-    // Remove the original instruction after replacing the aliases.
-    // pos.goto_inst(inst);
-    // let _removed_inst: Inst = pos.remove_inst();
-    // pos.goto_inst(select_inst);
 }
