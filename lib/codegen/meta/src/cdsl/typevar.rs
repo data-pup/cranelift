cf18366c6e932775c2292b3d27af007b7de56bb1
data-pup <kevinmartinucr@gmail.com>
2018-08-31 17:03:35 -0400

    Added typevar statics.

--- lib/codegen/meta/src/cdsl/typevar.rs
+++ lib/codegen/meta/src/cdsl/typevar.rs
@@ -3,4 +3,8 @@
 //! Cranelift instructions and instruction transformations can be specified to be
 //! polymorphic by using type variables.
 
+static _MAX_LANES: u32 = 256;
+static _MAX_BITS: u32 = 64;
+static _MAX_BITVEC: u32 = 16_384;
+
 pub struct _TypeVar;
